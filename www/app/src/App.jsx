98b2f9a18e301aa56d8560f6a9fe0c50aece7708
Michael McGovern <michaelmcgovern120@gmail.com>
2023-05-21 17:47:05 -0400

    Initialize front end with title

---
+++ www/app/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="text-center py-6 mb-5">
+      <h1 className="font-bold text-4xl text-transparent bg-clip-text bg-gradient-to-b from-purple-200 to-cyan-300 drop-shadow-lg">
+        Pokemon Quantity App
+      </h1>
+    </div>
+  );
+}
+
+export default App
